2e37159414e22a9dc1db38031062b6e21e5e3848
sasinha <sasinha@ucsc.edu>
2017-03-02 01:16:24 -0800

    Got inverse to work. Edited matrix to image functon

--- app2.py
+++ app2.py
@@ -6,8 +6,30 @@ from modules import modulo_inverse_matrix as mim
 
 color_mod = 255
 
+def image_to_matricies(image):
+    width, height = image.size
+    pix = image.load()
+    im_r = np.empty([height, width], dtype=int)
+    im_g = np.empty([height, width], dtype=int)
+    im_b = np.empty([height, width], dtype=int)
+
+    y = 0
+    while y < height:
+
+        x = 0
+        while x < width:
+            im_r[x,y] = (pix[x, y])[0]
+            im_g[x,y] = (pix[x, y])[1]
+            im_b[x,y] = (pix[x, y])[2]
+            x += 1
+        y += 1
+
+    return (im_r, im_g, im_b)
+
+
+
 def matricies_to_image(rgb):
-    (height, width) = rgb[0].shape
+    (width, height) = rgb[0].shape
     image = Image.new("RGB", (width, height), (255, 255, 255))
     pix = image.load()
 
@@ -29,7 +51,6 @@ def random_color_matrix(n):
     return mim.random_mod_matrix(0, color_mod, (n,n)), mim.random_mod_matrix(0, color_mod, (n,n)), mim.random_mod_matrix(0, color_mod, (n,n))
 
 
-
 def encrypt(image):
     n = image.size[1]
 
@@ -38,18 +59,24 @@ def encrypt(image):
     key_matricies = inv_color_matrix(cipher_matricies)
     test = inv_color_matrix(key_matricies)
     test_im = matricies_to_image(test)
-    print(cipher_matricies[2])
-    print(test[2])
 
     cipher_image = matricies_to_image(cipher_matricies)
     key_image = matricies_to_image(key_matricies)
-    return cipher_image, key_image, test_im
+    return cipher_image, key_image
 
 # Testing
 if __name__ == "__main__":
     im = Image.open("Images/2xtest.png")
-    (scr, key, check) = encrypt(im)
-    check.save("TestResults/check.png")
+    # pix = im.load()
+    # print(pix[0,0])
+    # print(pix[0,1])
+    # print(pix[1,0])
+    # print(pix[1,1])
+    # f= image_to_matricies(im)
+    # print(f[0])
+    # print(f[1])
+    # print(f[2])
+    (scr, key) = encrypt(im)
     scr.save("TestResults/scr.png")
     key.save("TestResults/key.png")
     # outAnswer = decrypt(scr, key)
